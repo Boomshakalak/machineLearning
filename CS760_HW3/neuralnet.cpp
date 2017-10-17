b32b16764ce217f2b1d0e9d6b47a6c4e88f28adc
Zhongya Wang <zwang895@wisc.edu>
2017-10-17 16:41:36 -0500

    hello

--- CS760_HW3/neuralnet.cpp
+++ CS760_HW3/neuralnet.cpp
@@ -17,26 +17,53 @@ int n_fold;
 int n_epoch;
 double label_distribution;
 vector<instance> data;
-vector<int> Asubset; //store index of data with first label
-vector<int> Bsubset; // store index of data with second label
+unordered_set<int> Asubset; //store index of data with first label
+unordered_set<int> Bsubset; // store index of data with second label
+
+/*
+	weights for input layers w1, and for hidden layer w2
+
+*/
+vector<vector<double>> w1;
+vector<double> w2;
 
 void readData(const string file,bool train);
 void getData(const string line);
 int findNextComma(string s, int cur);
 void inline lb_dis(const vector<instance>& data){
 	for (size_t i = 0 ; i < data.size(); i++){
-		if (data[i].label) Bsubset.push_back(i);
-		else Asubset.push_back(i);
+		if (data[i].label) Bsubset.insert(i);
+		else Asubset.insert(i);
 	}
 	label_distribution = double(Asubset.size())/data.size();
+}
+void initWeight(){
+	w1 = vector<vector<double>>(tp.feature.size(),vector<double>(tp.feature.size()+1));
+	w2 = w1[0];
+	for (auto& x : w1){
+		for (auto& w : x){
+			// srand(time(NULL));
+			w = double(rand()%1000000-500000)/500000;
+		}
+	}
+	for (auto& w : w2){
+		// srand(time(NULL));
+		w = double(rand()%1000000-500000)/500000;
+	}
 }
 int main(int argc, char const *argv[])
 {
 	/* code */
 	readData("sonar.arff", true);
+	initWeight();
 	lb_dis(data); // calculate the data distribution for the overall all data and store it in global variables
-	cout<<Asubset.size()<<endl;
-	cout<<Bsubset.size()<<endl;
+	initWeight();
+	for (auto x : w1[3])cout<<x<<"  ";
+	cout<<endl;
+	for (auto x : w2)cout<<x<<" ";
+	cout<<endl;
+	// cout<<Asubset.size()<<endl;
+	// cout<<Bsubset.size()<<endl;
 	cout<<label_distribution<<endl;
 	n_fold = 20;
 	n_epoch = 5;
